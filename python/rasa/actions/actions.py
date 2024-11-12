00eccc28d9ce2352298372f2a98dc8d26d417e29
Piotr-Lenarczyk <85508623+Piotr-Lenarczyk@users.noreply.github.com>
2024-11-12 10:41:19 +0100

    Stories improvements

--- python/rasa/actions/actions.py
+++ python/rasa/actions/actions.py
@@ -41,13 +41,27 @@ class ActionDisplayOpeningHours(Action):
         dispatcher.utter_message(text="Certainly! Here are the opening hours:")
         with open('opening_hours.json') as f:
             contents = json.load(f)
-            for item in contents['items']:
-                if contents['items'][item]['open'] == 0 and contents['items'][item]['close'] == 0:
-                    text = str(item) + ": closed"
-                else:
-                    text = str(item) + ": " + str(contents['items'][item]['open']) + " - " + str(contents['items'][item]['close'])
-                dispatcher.utter_message(text=text)
+            day = tracker.get_slot("day")
+            if not day:
+                for item in contents['items']:
+                    if contents['items'][item]['open'] == 0 and contents['items'][item]['close'] == 0:
+                        text = str(item) + ": closed"
+                    else:
+                        text = str(item) + ": " + str(contents['items'][item]['open']) + " - " + str(
+                            contents['items'][item]['close'])
+                    dispatcher.utter_message(text=text)
+            else:
+                for item in contents['items']:
+                    if str(item).lower() == str(day).lower():
+                        if contents['items'][item]['open'] == 0 and contents['items'][item]['close'] == 0:
+                            text = str(item) + ": closed"
+                        else:
+                            text = str(item) + ": " + str(contents['items'][item]['open']) + " - " + str(
+                                contents['items'][item]['close'])
+                        dispatcher.utter_message(text=text)
+                        break
             return []
+
 
 class DisplayMenuAction(Action):
 
@@ -62,8 +76,37 @@ class DisplayMenuAction(Action):
             for item in contents['items']:
                 text = str(item['name']) + ": " + str(item['price']) + " PLN"
                 dispatcher.utter_message(text=text)
-                dispatcher.utter_message(text="Extra ingredients:")
-                for ingredient in item['ingredients']:
-                    text = "\t- " + str(ingredient['name'])
-                    dispatcher.utter_message(text=text)
         return []
+
+
+class OrderFoodAction(Action):
+
+    def name(self) -> Text:
+        return "action_order_food"
+
+    async def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: "DomainDict") -> List[
+        Dict[Text, Any]]:
+        with open('menu.json') as f:
+            contents = json.load(f)
+            meal_choice = tracker.get_slot("meal")
+            extras = tracker.get_slot("extras")
+            exclusions = tracker.get_slot("exclusions")
+            if not meal_choice:
+                dispatcher.utter_message(text="You haven't ordered anything, would you like to order again?")
+                return []
+            for meal in contents['items']:
+                if str(meal['name']).lower() == meal_choice.lower():
+                    text = "Registered " + str(meal_choice)
+                    if not extras:
+                        dispatcher.utter_message("No extras registered")
+                    else:
+                        text += ", extra " + str(extras)
+                    if not exclusions:
+                        dispatcher.utter_message("No exclusions registered")
+                    else:
+                        text += ", no " + str(exclusions)
+                    dispatcher.utter_message(text=text)
+                    return []
+            dispatcher.utter_message(text="I'm sorry, I couldn't find " + str(meal_choice) + "in the menu. Would you "
+                                                                                             "like to order again?")
+            return []
